7aa4a68a2944d5a84613d8e621c7c210e7ec654d
Manish Singh Suyal <96225348+Manishsuyal31@users.noreply.github.com>
2022-05-30 22:58:05 +0530

    useState

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,18 +1,21 @@
 import { useContext } from 'react'
+import { BookContext } from '../context/BookContext';
 import { ThemeContext } from '../context/ThemeContext';
 
 const Booklist = () => {
 
-    const context = useContext(ThemeContext);
-    const { isLightTheme, light, dark } = context;
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const { books } = useContext(BookContext);
     const theme = isLightTheme ? light : dark;
 
     return (
         <div className='book-list' style={{ color: theme.syntax, background: theme.bg }}>
             <ul>
-                <li style={{ background: theme.ui }}>The way of kings</li>
-                <li style={{ background: theme.ui }}>The name of the wind</li>
-                <li style={{ background: theme.ui }}>The final empire</li>
+                {books.map((book) => {
+                    return (
+                        <li key={book.id} style={{ background: theme.ui }}>{ book.title }</li>
+                    )
+                })}
             </ul>
         </div>
     )
